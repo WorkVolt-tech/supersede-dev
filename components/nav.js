d9fd12095229d9e7ec5d8984e44609a50111cde8
WorkVolt-tech <ancmarx@gmail.com>
2026-05-08 00:12:19 -0400

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -67,7 +67,7 @@ export async function renderNav(containerId = 'nav') {
         gap: 0;
       }
       .bm-left {
-        width: 200px;
+        width: 100px;
         flex-shrink: 0;
         background: url('${base}components/left_cap.webp') no-repeat center center;
         background-size: 100% 100%;
