f0d21e7ffc01cc463b5512f9b6e46e163a1e1fff
WorkVolt-tech <ancmarx@gmail.com>
2026-05-09 11:24:52 -0400

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -62,7 +62,7 @@ export async function renderNav(containerId = 'nav') {
         width: 770px;
         height: 250px;
         margin: 0 auto;
-        margin-top: 80px;
+        margin-top: 150px;
         user-select: none;
       }
       .bm-img {
